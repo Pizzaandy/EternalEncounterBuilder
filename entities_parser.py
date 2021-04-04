9777575c97a26ad238dddd3e5706fdfecf1ea163
Pizzaandy <47232037+Pizzaandy@users.noreply.github.com>
2021-04-04 12:33:45 -0500

    saving stuff(?)

--- entities_parser.py
+++ entities_parser.py
@@ -7,6 +7,8 @@ from multiprocessing import Pool
 import multiprocessing as mp
 from parsimonious.grammar import Grammar
 from parsimonious.grammar import NodeVisitor
+import chevron
+from textwrap import dedent
 
 entity_grammar = Grammar(r"""
     #DOCUMENT = VERSION_LINES? ENTITY*
@@ -39,6 +41,36 @@ entity_grammar = Grammar(r"""
     SPACE   = ~r"\s+"
 """)
 
+ebl_grammar = Grammar(r"""
+    DOCUMENT = (STATEMENTS)*
+    STATEMENTS = SPACE? (EVENT / WAVE / WAITFORBLOCK / WAITFOR)
+    
+    WAVE = "Wave" SPACE STRING LBRACE STATEMENTS* RBRACE
+    PARAM_LIST = LBRACKET PARAM_TUPLE* RBRACKET
+    PARAM_TUPLE = LPARENTHESES PARAM_LINE RPARENTHESES SPACE? ","? SPACE?
+    PARAM_LINE = PARAM+
+    PARAM = SPACE? (NUMBER / STRING / BOOL / SPACE) SPACE? ("," / &RPARENTHESES)
+    
+    EVENT = STRING SPACE? LPARENTHESES (PARAM_LIST / PARAM_LINE)? RPARENTHESES
+    
+    WAITFORBLOCK = "waitFor" LBRACE EVENT* RBRACE
+    WAITFOR = "waitFor" (EVENT / TIMER)
+    TIMER = NUMBER SPACE? "sec"
+        
+    LBRACE    = SPACE? "{" SPACE?
+    RBRACE    = SPACE? "}" SPACE?
+    LBRACKET  = SPACE? "[" SPACE?
+    RBRACKET  = SPACE? "]" SPACE?
+    LPARENTHESES  = SPACE? "(" SPACE?
+    RPARENTHESES  = SPACE? ")" SPACE?
+    
+    SPACE = ~r"\s+"
+    STRING = ~r"\w+"
+    NUMBER = ~r"[+\-]?(?:0|[1-9]\d*)(?:\.\d*)?(?:[eE][+\-]?\d+)?"
+    COMMENT = ~r"//(.*)[\r\n]+"
+    BOOL = "true" / "false"
+""")
+
 
 class EntityVisitor(NodeVisitor):
     def parse(self, text):
@@ -54,7 +86,6 @@ class EntityVisitor(NodeVisitor):
     
     def visit_VERSION_LINES(self, node, visited_children):
         _, _, version, _, _, _, hierarchy_version, _ = visited_children
-        
         self.version = version
         self.hierarchy_version = hierarchy_version
         print(f"Version:{version}, HierarchyVersion:{hierarchy_version}")
@@ -89,7 +120,7 @@ class EntityVisitor(NodeVisitor):
     def visit_INDEXED(self, node, visited_children):
         varname, _, index, _ = visited_children
         return f"{varname}[{index}]"
- 
+    
     def visit_VARNAME(self, node, visited_children):
         return str(node.text)
     
@@ -113,28 +144,119 @@ class EntityVisitor(NodeVisitor):
         return visited_children or node
 
 
+class EBLVisitor(NodeVisitor):
+    def visit_DOCUMENT(self, node, visited_children):
+        event_list = visited_children
+        return event_list
+    
+    def visit_STATEMENTS(self, node, visited_children):
+        _, statement = visited_children
+        return statement[0]
+    
+    def visit_WAVE(self, node, visited_children):
+        _, _, varname, _, statements, _ = visited_children
+        if not isinstance(statements, list):
+            return ["WAVE: " + str(varname)]
+        return statements.insert(0, "WAVE: " + str(varname))
+    
+    def visit_PARAM(self, node, visited_children):
+        _, value, _, _ = visited_children
+        return value[0]
+    
+    def visit_PARAM_LINE(self, node, visited_children):
+        params = visited_children
+        print(params)
+        return params
+    
+    def visit_PARAM_TUPLE(self, node, visited_children):
+        _, param_line, _, _, _, _ = visited_children
+        return param_line[0]
+    
+    def visit_PARAM_LIST(self, node, visited_children):
+        _, param_tuples, _ = visited_children
+        return param_tuples
+    
+    def visit_EVENT(self, node, visited_children):
+        event_name, _, _, params, _ = visited_children
+        if not isinstance(params, list):
+            return ["EVENT: " + str(event_name)]
+        return params.insert(0, "EVENT: " + str(event_name))
+    
+    def visit_WAITFOR(self, node, visited_children):
+        _, conditions = visited_children
+        return conditions.insert(0, "WAITFOR")
+    
+    def visit_WAITFORBLOCK(self, node, visited_children):
+        _, conditions = visited_children
+        return conditions.insert(0, "WAITFORBLOCK")
+    
+    def visit_TIMER(self, node, visited_children):
+        duration, _, _ = visited_children
+        return duration[0]
+    
+    def visit_STRING(self, node, visited_children):
+        return str(node.text)
+    
+    def visit_NUMBER(self, node, visited_children):
+        return float(node.text)
+    
+    def visit_BOOL(self, node, visited_children):
+        return node.text == "true"
+    
+    def generic_visit(self, node, visited_children):
+        return visited_children or node
+
+
 ev = EntityVisitor()
 ev.grammar = entity_grammar
+
+def strip_comments(string):
+    pattern = r"//(.*)[\r\n]+"
+    return re.sub(pattern, "", string)
 
 def generate_segments(filename):
     with open(filename) as fp:
         segments = re.split(r"^entity {", fp.read(), flags=re.MULTILINE)
     # skip first segment with version numbers in it, remove comments
+    segment_count = 0
     for segment in segments[1:]:
+        segment = strip_comments(segment)
         # handle encounters only for now
-        if "idEncounterManager" in segment:
+        if '''inherit = "target/spawn";''' in segment:
+            segment_count += 1
             yield "entity {" + re.sub(r"//.*$", "", segment)
+            
+    print(f"{segment_count} targets found")
 
 def convert_entities_file(filename):
     tic = time.time()
+    data = []
     if __name__ == '__main__':
         print("Start processing")
         with Pool(processes=mp.cpu_count()) as pool:
             data = pool.map(ev.parse, generate_segments(filename))
          
         print(f"Done processing in {time.time()-tic:.1f} seconds")
-         
         with open('testoutput.json', 'w') as fp:
             json.dump(data, fp, indent = 4)
+    return data
 
-convert_entities_file("example_entities.txt")
+
+sample_txt = ("""
+Wave bruh {
+}
+eee()
+""")
+
+
+ebl = EBLVisitor()
+ebl.grammar = ebl_grammar
+#data = ebl_grammar.parse(sample_txt)
+data = ebl.parse(sample_txt)
+
+print(data)
+
+
+
+
+
--- eternalevents.py
+++ eternalevents.py
@@ -1,8 +1,26 @@
 import chevron
 from textwrap import dedent
+import sys
+
+def str_to_class(classname):
+    return getattr(sys.modules[__name__], classname)
 
 # bruh
 camelcase = lambda test_str: test_str[:1].lower() + test_str[1:] if test_str else '' 
+
+# (alias, total_args, optional_args)
+event_to_ebl = {}
+ebl_to_event = {}
+
+encounterSpawnNames = [
+    "ARACHNOTRON",
+    "BARON",
+    "CACODEMON",
+    "CUEBALL",
+    "CYBER_MANCUBUS",
+    "DOOM_HUNTER",
+    "DREAD_KNIGHT"
+]
 
 class EternalEvent:
     template = dedent("""\
@@ -30,16 +48,25 @@ class EternalEvent:
         ]
         return chevron.render(template=self.template, data={"name":camelcase(type(self).__name__), "items":items, "count":len(items)})
     
-    def __init_subclass__(cls, **kwargs):
+    def __init_subclass__(cls, alias="", **kwargs):
         super().__init_subclass__(**kwargs)
+        # initialize fields
         args = []
+        optional_args = 0
         for attr, value in cls.__dict__.items():
             if not attr.startswith('__'):
+                if value[-1] == "*":
+                    value = value.replace("*", "")
+                    optional_args += 1
                 args.append((attr,value))
         cls.args = args
+        # add alias to dicts
+        _alias = alias if alias else camelcase(cls.__name__)
+        event_to_ebl[cls.__name__] = (_alias, len(args), optional_args)
+        ebl_to_event[_alias] = cls.__name__
         
     def __str__(cls):
-            return cls.stringify()
+        return cls.stringify()
         
     def __init__(self, *args):
         for init_arg, (cls_name, cls_args) in zip(args, self.args):
@@ -61,6 +88,11 @@ class SpawnAI(EternalEvent):
     spawn_count: str = "int"
     spawnGroup: str = "idTargetSpawnGroup"
     group_label: str = "char"
+    
+class SpawnSingleAI(EternalEvent, alias = "spawn"):
+    spawnType: str = "eEncounterSpawnType_t"
+    spawnTarget: str = "idTarget_Spawn"
+    group_label: str = "char*"
         
 class SetMusicState(EternalEvent):
     target: str = "idMusicEntity"
@@ -71,8 +103,20 @@ class Wait(EternalEvent):
     seconds: str = "float"
     disableAIHighlight: str = "bool"
         
-class WaitAIHealthLevel:
+class WaitAIHealthLevel(EternalEvent):
     aiType: str = "eEncounterSpawnType_t"
     desired_remaing_ai_count: str = "int"
     group_label: str = "char"
     disableAIHighlight: str = "bool"
+
+class ActivateTarget(EternalEvent):
+    targetEntity: str = "entity"
+    command: str = "Lock Door"
+
+#events = []
+#for line in lines:
+    #if line.startswith("Wave"):
+        
+
+
+
